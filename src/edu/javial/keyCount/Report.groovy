2c0eea00310af0351e902ce0fac3a98cae753ec7
Bob Makowski <tritonpi-git0@yahoo.com>
2014-07-17 21:51:04 -0500

    T : version 1

--- src/edu/javial/keyCount/Report.groovy
+++ src/edu/javial/keyCount/Report.groovy
@@ -1,34 +1,60 @@
 package edu.javial.keyCount
 
 /**
- * Created by mak on 7/15/14.
+ * Created by Bob Makowski on 7/15/14.
  */
 class Report {
 
-  def location = "./var/testData/keyCount"
-  Report( String whereData ) { location = whereData ?: location }
+  static def defaultLocation = "./var/testData/keyCount"
+  def location = defaultLocation
 
-  def List fetchFileContentsAsList() {
-    new File(location).readLines()
-  }
+  Report(String whereData) { location = whereData ?: defaultLocation }
 
-  def fetchAndProcessFileContentsAsKeyCounts = {
-      def done = fetchFileContentsAsList().inject( [:] , processFileContentsAsKeyCount )
-  }
-  def processFileContentsAsKeyCount = { cache, l ->
-    def m = [:]
-    def keyAndValue = l.split(',')
-    def key = keyAndValue[0]
-    m[ key ] = ( keyAndValue[1] as Integer )
-    def last = cache[key ] ?: 0
-    cache[key] = last + m[key]
-    return cache
-  }
   def reportFileContentsAsKeyCounts = {
-    def done = fetchAndProcessFileContentsAsKeyCounts()
-    reportFileContentsKeyCounts()
+    def reportingMap = fetchAndProcessFileContentsAsKeyCounts()
+    reportFileContentsKeyCounts reportingMap
   }
-  def reportFileContentsKeyCounts = {
-//    cache.each { k, v -> println "The total for ${k} is ${v}" }
+
+  /**
+   * fetches the content line by line, parses each line, and maps the key, value pairs.
+   */
+  def fetchAndProcessFileContentsAsKeyCounts = {
+    fetchFileContentsAsList().inject([:], processFileContentsAsKeyCount)
+  }
+
+  /**
+   * converts file into a List of lines
+   */
+  def List fetchFileContentsAsList() {
+    verifiedFileLocation(location).readLines()
+  }
+
+  /**
+   * parses a line, and maps the key, value pairs.
+   */
+  def processFileContentsAsKeyCount = { Map keyCountMap, String line ->
+    String[] parsedLine = line.split(",")
+    validateParsedLine(parsedLine)
+    String key = parsedLine[0]
+    Integer value = parsedLine[1] as Integer
+    keyCountMap[key] = (keyCountMap[key] ?: 0) + value
+    return keyCountMap
+  }
+  def reportFileContentsKeyCounts = { reportingMap ->
+    reportingMap.each { k, v -> println "The total for ${k} is ${v}" }
+  }
+
+  def validateParsedLine( String[] tokenArray ) {
+    assert null != tokenArray
+    assert null != tokenArray[0]
+    assert null != tokenArray[1]
+  }
+
+  def File verifiedFileLocation( String path ) {
+    assert null != path
+    File f = new File(path)
+    assert f.exists()
+    assert f.file
+    return f
   }
 }
