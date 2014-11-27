da9fa262fa9b0a8a293d7bd78fa3e669b3e5efc1
Bob Makowski <tritonpi-git0@yahoo.com>
2014-11-27 09:07:15 -0600

    initial checkin : post maven archetype

---
+++ src/test/groovy/jh/thoughtworks/HelperTest.groovy
@@ -0,0 +1,19 @@
+//
+// Generated from archetype; please customize.
+//
+
+package jh.thoughtworks
+
+import jh.thoughtworks.Helper
+import jh.thoughtworks.Example
+
+/**
+ * Tests for the {@link Helper} class.
+ */
+class HelperTest
+    extends GroovyTestCase
+{
+    void testHelp() {
+        new Helper().help(new Example())
+    }
+}
