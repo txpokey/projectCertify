25c9136331b4edcdfcb1fb542ab6a7d403386fd3
Bob Makowski <tritonpi-git0@yahoo.com>
2014-11-27 09:07:15 -0600

    initial checkin : post maven archetype

---
+++ src/test/groovy/jh/thoughtworks/ExampleTest.groovy
@@ -0,0 +1,18 @@
+//
+// Generated from archetype; please customize.
+//
+
+package jh.thoughtworks
+
+import jh.thoughtworks.Example
+
+/**
+ * Tests for the {@link Example} class.
+ */
+class ExampleTest
+  extends GroovyTestCase {
+
+  void testShow() {
+    new Example().show()
+  }
+}
