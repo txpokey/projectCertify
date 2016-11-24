e74d73fcd4f5f86e2e5ca29568ae4c969b5f0151
Bob Makowski <tritonpi-git0@yahoo.com>
2016-11-23 23:08:28 -0600

    porting to intel-nuc.punisher

--- src/test/groovy/jh/thoughtworks/conferencer/biz/EventComparatorTest.groovy
+++ src/test/groovy/jh/thoughtworks/conferencer/biz/EventComparatorTest.groovy
@@ -9,16 +9,21 @@ import jh.thoughtworks.conferencer.domain.Event
  * Tests for the {@link EventComparator} class.
  */
 class EventComparatorTest extends GroovyTestCase {
-  EventComparator tool
-
+  EventComparator tool = new EventComparator()
+  def events
+  def ref
+  def probe
   void setUp() {
     super.setUp()
     tool = new EventComparator()
+    events = [ seed0, less0, equal0, more0 ].collect { new Event(it)}
+    ref = events[0]
+    probe = events[1..3]
   }
+  //    def events = [ seed0, less0, equal0, more0 ].collect { new Event(it)}
+//    def ref = events[0]
+//    def probe = events[1..3]
   void testCompare() {
-    def events = [ seed0, less0, equal0, more0 ].collect { new Event(it)}
-    def ref = events[0]
-    def probe = events[1..3]
     // can't use the combinations() method on the iterator because of comparator.
     // so doing it 'by hand' here with the injector accumulating combos
     def data = probe.inject( [] , { all , it -> all << [ ref , it ] })
@@ -35,6 +40,11 @@ class EventComparatorTest extends GroovyTestCase {
   void testCompareBoundaries() {
     shouldFail { tool.compare( null, new Event(seed0)) }
     shouldFail { tool.compare(  new Event(seed0), null) }
+  }
+  void testSorting() {
+    TreeMap<Event, String> tree = new TreeMap( tool )
+    events.each { it -> tree.put( it , it.line ) }
+    print tree.values().dump()
   }
   final Map less0 = [
     line : 'less0' ,
@@ -59,7 +69,7 @@ class EventComparatorTest extends GroovyTestCase {
   ]
   final Map seed0 = [
     line : 'seed0' ,
-    normalized : 'BAR' ,
+    normalized : 'seed0' ,
     minutes : 22 ,
     session : 5 ,
     track : 3
