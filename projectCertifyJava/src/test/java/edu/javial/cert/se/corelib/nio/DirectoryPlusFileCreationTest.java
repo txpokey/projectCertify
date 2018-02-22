d4e74fd272457c1c888570a6cfca19ef78c4961e
Bob Makowski <tritonpi-git0@yahoo.com>
2018-02-21 21:56:02 -0600

    F:T:-: DirectoryPlusFileCreationTest does directories with tearDown. check TODO herein

--- projectCertifyJava/src/test/java/edu/javial/cert/se/corelib/nio/DirectoryPlusFileCreationTest.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/corelib/nio/DirectoryPlusFileCreationTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package edu.javial.cert.se.corelib.nio;
 
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
@@ -14,88 +15,104 @@ import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author mak
- * TODO : Path is for special filesystem development, and part of NIO.
- * TODO : refactor for File and Files.
- * TODO : create actual tests of file and directory creation
- * DONE : create tests showing round-trip equivalence of Path and File
+ *         TODO : Path is for special filesystem development, and part of NIO.
+ *         TODO : INWORK : refactor for File and Files.
+ *         DONE : create actual tests directory creation
+ *         TODO : create actual tests of file creation
+ *         DONE : create tests showing round-trip equivalence of Path and File
  */
 public class DirectoryPlusFileCreationTest {
-	private static Log log = LogFactory.getLog(DirectoryPlusFileCreationTest.class);
-	private static final String FILE_ERROR_MSG = "unable to create file";
-	private static final String DIR_ERROR_MSG = "unable to create directory";
-	private static final String RENAME_ERROR_MSG = "unable to rename";
-    private static final Set<PosixFilePermission> perms = PosixFilePermissions.fromString("rwxr-x---" );
-    private static final FileAttribute[] attrs = new FileAttribute[] {PosixFilePermissions.asFileAttribute(perms)};
+    private static Log log = LogFactory.getLog(DirectoryPlusFileCreationTest.class);
+    private static final String FILE_ERROR_MSG = "unable to create file";
+    private static final String DIR_ERROR_MSG = "unable to create directory";
+    private static final String RENAME_ERROR_MSG = "unable to rename";
+    private static final Set<PosixFilePermission> perms = PosixFilePermissions.fromString("rwxr-x---");
+    private static final FileAttribute[] attrs = new FileAttribute[]{PosixFilePermissions.asFileAttribute(perms)};
 
-    private static final String SEEDED_DIRNAME = "sciDir";
-    private static final String SEEDED_FILENAME = "sciFile";
+    private static final String SLASH = File.separator;
+//    private static final String SEEDED_DIRNAME = "/tmp/sciDir" + Math.random() + SLASH + Math.random();
+//    private static final String SEEDED_FILENAME = "sciFile" + Math.random();
+    private static final Path SEEDED_AS_PATH = generateRandomPathFromSeed();
+    private static final Path SEEDED_PATH_PARENT = SEEDED_AS_PATH.getParent();
+    private static final File SEEDED_AS_FILE = generateRandomPathFromSeed().toFile();
+    private static final File SEEDED_FILE_PARENT = SEEDED_AS_FILE.getParentFile();
+
     //
-    ////            Path path = Paths.get(requestedDirectory, requestedFileName);
-    //
-    boolean createDirThenFile() {
-        boolean ret = false;
-        boolean dirOK = false;
-        boolean fileOK = false;
-        Path dirPath = privateCreateTempDir() ;
-        Path completedPath = dirPath.resolve(SEEDED_FILENAME);
-        Path createdFileAsPath = privateCreateFileFromPath(completedPath);
-        Object d0 = createdFileAsPath.getParent() ;
-        Object n0 = createdFileAsPath.getFileName();
-        File f = createdFileAsPath.toFile() ;  // TODO exception
-        Object d = f.getParent() ;
-        Object n =f.getName();
-        Object p = f.toPath();
-        return null != f ;
+//    @AfterAll
+//    static void tearDownAll() {
+//        log.info("HERE");
+//        tearDown_testCreateDirectoryByRequestedPath() ;
+//        tearDown_testCreateDirectoryByRequestedFile() ;
+//    }
+    private static Path generateRandomPathFromSeed() {
+        String dn = "/tmp/sciDir" + Math.random() + SLASH + Math.random();
+        String fn = "sciFile" + Math.random();
+        return Paths.get(dn, fn) ;
     }
-    private Path privateCreateTempDir() {
+
+    @Test
+    public void testCreateDirectoryByRequestedPath() {
+        assertNotNull(SEEDED_PATH_PARENT);
+        Path returnedPath = privateCreateDirectory(SEEDED_PATH_PARENT);
+        assertNotNull(returnedPath);
+        assertEquals(SEEDED_PATH_PARENT.toString(), returnedPath.toString());
+        assert( tearDown_testCreateDirectoryByRequestedPath() );
+    }
+
+    @Test
+    public void testCreateDirectoryByRequestedFile() {
+        assertNotNull(SEEDED_FILE_PARENT);
+        File returnedFile = privateCreateDirectory(SEEDED_FILE_PARENT);
+        assertNotNull(returnedFile);
+        assertEquals(SEEDED_FILE_PARENT.toString(), returnedFile.toString());
+        assert( tearDown_testCreateDirectoryByRequestedFile() );
+    }
+
+    private Path privateCreateDirectory(Path requestedParentPath) {
+        assertNotNull(requestedParentPath);
         Path ret = null;
         try {
-            Path path = Files.createTempDirectory(SEEDED_DIRNAME, attrs);
-            Path absPath = path.toAbsolutePath();
-            log.debug(absPath.toString());
-            ret = path;
+            Path p = Files.createDirectories(requestedParentPath, attrs);
+            assertNotNull(p);
+            ret = p;
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return ret ;
-    }
-
-    private Path privateCreateFileFromPath(Path requestedPath) {
-        Path ret = null ;
-        create : {
-            try {
-                Path temp = Files.createFile(requestedPath,attrs);
-                ret = temp;
-            } catch (IOException e) {
-                log.fatal("failed on creating file from requested path:> " + requestedPath);
-            }
+            log.debug("cant create requested directory:> " + requestedParentPath, e);
         }
         return ret;
     }
 
-    boolean createTempFile() {
-		boolean ret = false;
-		File temp = privateCreateTempFile(SEEDED_DIRNAME);
-        ret = null != temp;
-        return ret ;
-	}
-
-    private File privateCreateTempFile(String requestedSeed) {
-        File ret = null;
-        create: {
-            try {
-                File temp = File.createTempFile(requestedSeed, null);
-                String path = temp.getAbsolutePath();
-                log.debug(path);
-                ret = temp ;
-            } catch (Exception e) {
-                log.fatal(FILE_ERROR_MSG, e);
-                break create;
-            }
-        } // create
+    private File privateCreateDirectory(File requestedParentFile) {
+        assertNotNull(requestedParentFile);
+        boolean candidate = requestedParentFile.mkdirs() ;
+        File ret = candidate ? requestedParentFile : null ;
         return ret;
+    }
+    private static boolean tearDown_testDirectory( Path requestedParentPath ) {
+        assertNotNull(requestedParentPath);
+        boolean tornDown = false ;
+        try {
+            boolean candidate1 = Files.deleteIfExists(requestedParentPath);
+            boolean candidate0 = Files.deleteIfExists(requestedParentPath.getParent());
+            tornDown = candidate0 && candidate1 ;
+        } catch (IOException e) {
+            log.debug("cant delete requested directory:> " + requestedParentPath, e);
+        }
+        return tornDown ;
+    }
+    private static boolean tearDown_testCreateDirectoryByRequestedPath(){
+        boolean tornDown = tearDown_testDirectory(SEEDED_PATH_PARENT) ;
+        return tornDown ;
+    }
+    private static boolean tearDown_testCreateDirectoryByRequestedFile(){
+        boolean tornDown = tearDown_testDirectory(SEEDED_FILE_PARENT.toPath()) ;
+        return tornDown ;
     }
 }
