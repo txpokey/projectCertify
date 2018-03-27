c6d66d6836bbc4246145175b342855771dcdbc82
Bob Makowski <tritonpi-git0@yahoo.com>
2018-03-26 19:05:48 -0500

    T:T:-: autoboxing improved with roundtrip test, using @NonNull and IntStream.range

--- projectCertifyJava/src/test/java/edu/javial/cert/se/sx/AutoboxFeatureTest.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/sx/AutoboxFeatureTest.java
@@ -2,9 +2,12 @@ package edu.javial.cert.se.sx;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
+import javax.annotation.Nonnull;
 import java.util.Optional;
+import java.util.stream.IntStream;
+
 
 public class AutoboxFeatureTest<T> {
     private static Log log = LogFactory.getLog(AutoboxFeatureTest.class);
@@ -16,14 +19,20 @@ public class AutoboxFeatureTest<T> {
         String candidate = dum.toString() ;
         return candidate;
     }
-    Optional<T> wanker ;
 
+// TODO nice to explore Optional here
 //    private String convertTypeToString( Optional<T> dum ) {
 //        String candidate = dum.orElse(T.class.newInstance()).toString() ; // SX: smoke
 //        return candidate;
 //    }
+
+    interface SampleContractForAutoBoxingIntegers {
+        Integer fromInt( int in ) ;
+        int fromInteger( Integer in );
+    }
+
     @Test
-    public void intToStringAutoBoxTest()
+    public void intToStringConvertTest()
     {
         int count = 1;
         Integer c = count;
@@ -36,7 +45,31 @@ public class AutoboxFeatureTest<T> {
 
         assert "1".equals(convertTypeToString((T)c));
 
-        ;
+    }
+    @Test
+    public void autoBoxIntegerTest() {
 
+        SampleContractForAutoBoxingIntegers sample = new SampleContractForAutoBoxingIntegers() {
+            @Override
+            public Integer fromInt(int in) {
+                Integer candidate = in ;
+                assert null != candidate ;
+                assert candidate.intValue() == in ;
+                return candidate;
+            }
+
+            @Override
+            public int fromInteger(@Nonnull Integer in) {
+                int candidate = in ;
+                assert Integer.valueOf(candidate).equals(in);
+                return candidate;
+            }
+        };
+        IntStream.range(0, 3).forEachOrdered(n -> {
+//            log.info( "result: " + sample.fromInt( n ));
+//            log.info( "result: " + sample.fromInteger( sample.fromInt(n) ));
+            int roundTripValue =  sample.fromInteger( sample.fromInt(n) ) ;
+            assert roundTripValue == n ;
+        });
     }
 }
