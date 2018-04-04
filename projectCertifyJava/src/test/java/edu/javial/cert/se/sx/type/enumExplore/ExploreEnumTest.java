fa777b1ef3b9c46d4aa9975462cd12b907ce2e8d
Bob Makowski <tritonpi-git0@yahoo.com>
2018-04-03 22:26:11 -0500

    F:T:-: clean up in work and some refactoring. TestNG DataProviders working with Inspector.inspect() and static peek.

--- projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/enumExplore/ExploreEnumTest.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/enumExplore/ExploreEnumTest.java
@@ -14,6 +14,13 @@ import java.util.stream.Stream;
 public class ExploreEnumTest {
     private static Log log = LogFactory.getLog(ExploreEnumTest.class);
 
+    interface Inspector<E extends Enum> {
+        default void inspect() {
+            E e = (E) this;
+            peek(e);
+        }
+    }
+
     enum AllDefaultValues implements Inspector {
         TRY0, TRY1
     }
@@ -39,42 +46,6 @@ public class ExploreEnumTest {
 
     }
 
-    interface EnumTestHarness<E extends Enum> {
-        default void inspect() {
-            E e = (E) this;
-            peek(e);
-        }
-    }
-
-    public void applyStaticPeekOverEntireEnumType0() {
-        log.debug("ENTER");
-        applyConsumerToEntireEnumConvertedToStream(AllDefaultValues.class, ExploreEnumTest::peek);
-        applyConsumerToEntireEnumConvertedToStream(DefineEnumValuesExample.class, ExploreEnumTest::peek);
-        log.debug("LEAVE");
-    }
-//    public void applyEnumHarnessInspectorOverEntireEnumType0() {
-//        log.debug("ENTER");
-//        applyConsumerToEntireEnumConvertedToStream(AllDefaultValues.class, EnumTestHarness::inspect);
-//        applyConsumerToEntireEnumConvertedToStream(DefineEnumValuesExample.class, EnumTestHarness::inspect);
-//        log.debug("LEAVE");
-//    }
-
-    private static <E extends Enum> void peek(E element) {
-        log.debug("name:> " + element.name());
-        log.debug("toString:> " + element.toString());
-        log.debug("ordinal:> " + element.ordinal());
-    }
-
-    private static <E extends Enum> void applyConsumerToEntireEnumConvertedToStream(@Nonnull Class<E> declare,
-                                                                                    @Nonnull Consumer<E> function) {
-        expressEnumTypeAsStream(declare).forEachOrdered(function);
-    }
-
-    private static <E extends Enum> Stream<E> expressEnumTypeAsStream(@Nonnull Class<E> declare) {
-        return Arrays.stream(declare.getEnumConstants());
-    }
-
-    //  DataProviders INWORK
 
     @DataProvider
     public final Object[][] getAllEnumTypesBeingTested() {
@@ -88,19 +59,34 @@ public class ExploreEnumTest {
         log.debug("LEAVE");
     }
 
-    // TODO : get DataProviders working on inspect scenario
-    interface Inspector<E extends Enum> {
-        default void inspect() {
-            E e = (E) this;
-            peek(e);
-        }
-    }
-
     @Test(dataProvider = "getAllEnumTypesBeingTested")
     public <E extends Enum> void applyInstanceMethodInspectOverEntireEnumType(Class<E> input) {
         log.debug("ENTER");
         Arrays.stream(input.getEnumConstants()).forEachOrdered(e -> ((Inspector<E>) e).inspect());
         log.debug("LEAVE");
+    }
+
+//
+//    public void applyStaticPeekOverEntireEnumType0() {
+//        log.debug("ENTER");
+//        applyConsumerToEntireEnumConvertedToStream(AllDefaultValues.class, ExploreEnumTest::peek);
+//        applyConsumerToEntireEnumConvertedToStream(DefineEnumValuesExample.class, ExploreEnumTest::peek);
+//        log.debug("LEAVE");
+//    }
+
+    private static <E extends Enum> void peek(E element) {
+        log.debug("name:> " + element.name());
+        log.debug("toString:> " + element.toString());
+        log.debug("ordinal:> " + element.ordinal());
+    }
+
+    private static <E extends Enum> void applyConsumerToEntireEnumConvertedToStream(@Nonnull Class<E> enumType,
+                                                                                    @Nonnull Consumer<E> function) {
+        expressEnumTypeAsStream(enumType).forEachOrdered(function);
+    }
+
+    private static <E extends Enum> Stream<E> expressEnumTypeAsStream(@Nonnull Class<E> enumType) {
+        return Arrays.stream(enumType.getEnumConstants());
     }
 
 }
