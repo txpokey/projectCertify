b274d7b13fda5ad7cea97862b4a315c63babb9d1
Bob Makowski <tritonpi-git0@yahoo.com>
2018-04-03 22:56:44 -0500

    T:T:-: ExploreEnumTest is working fine. 1st version

--- projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/enumExplore/ExploreEnumTest.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/enumExplore/ExploreEnumTest.java
@@ -45,48 +45,57 @@ public class ExploreEnumTest {
         private String asString;
 
     }
-
-
+ /*
+  ****************************** TESTING ******************************
+  */
     @DataProvider
     public final Object[][] getAllEnumTypesBeingTested() {
         return new Object[][]{{AllDefaultValues.class}, {DefineEnumValuesExample.class}};
     }
 
     @Test(dataProvider = "getAllEnumTypesBeingTested")
-    public <E extends Enum> void applyStaticPeekOverEntireEnumType(Class<E> input) {
+    public <E extends Enum> void applyStaticPeekOverEntireEnumType(Class<E> enumTypeAsInput) {
         log.debug("ENTER");
-        applyConsumerToEntireEnumConvertedToStream(input, ExploreEnumTest::peek);
+        applyConsumerToEntireEnumConvertedToStream(enumTypeAsInput, ExploreEnumTest::peek);
         log.debug("LEAVE");
     }
+
 
     @Test(dataProvider = "getAllEnumTypesBeingTested")
-    public <E extends Enum> void applyInstanceMethodInspectOverEntireEnumType(Class<E> input) {
+    public <E extends Enum> void applyInstanceMethodInspectOverEntireEnumType(Class<E> enumTypeAsInput) {
+        /*
+         * inspectorFunction as a work around :
+         * once I added @DataProviders I could no longer use Inspector::inspect as Consumer<E> in forEachOrdered()
+         *     cf. exampleUsingInspectorInspect()
+         */
+        final Consumer<E> inspectorFunction = e -> ((Inspector<E>) e).inspect() ;
         log.debug("ENTER");
-        Arrays.stream(input.getEnumConstants()).forEachOrdered(e -> ((Inspector<E>) e).inspect());
+        applyConsumerToEntireEnumConvertedToStream(enumTypeAsInput, inspectorFunction);
         log.debug("LEAVE");
     }
 
-//
-//    public void applyStaticPeekOverEntireEnumType0() {
-//        log.debug("ENTER");
-//        applyConsumerToEntireEnumConvertedToStream(AllDefaultValues.class, ExploreEnumTest::peek);
-//        applyConsumerToEntireEnumConvertedToStream(DefineEnumValuesExample.class, ExploreEnumTest::peek);
-//        log.debug("LEAVE");
-//    }
-
+    public <E extends Enum> void exampleUsingInspectorInspect() {
+        log.debug("ENTER");
+        applyConsumerToEntireEnumConvertedToStream(AllDefaultValues.class, Inspector::inspect);
+        applyConsumerToEntireEnumConvertedToStream(DefineEnumValuesExample.class, Inspector::inspect);
+        log.debug("LEAVE");
+    }
+    /*
+     ****************************** CONVENIENCE ******************************
+     */
     private static <E extends Enum> void peek(E element) {
         log.debug("name:> " + element.name());
         log.debug("toString:> " + element.toString());
         log.debug("ordinal:> " + element.ordinal());
     }
 
-    private static <E extends Enum> void applyConsumerToEntireEnumConvertedToStream(@Nonnull Class<E> enumType,
+    private static <E extends Enum> void applyConsumerToEntireEnumConvertedToStream(@Nonnull Class<E> enumTypeAsInput,
                                                                                     @Nonnull Consumer<E> function) {
-        expressEnumTypeAsStream(enumType).forEachOrdered(function);
+        expressEnumTypeAsStream(enumTypeAsInput).forEachOrdered(function);
     }
 
-    private static <E extends Enum> Stream<E> expressEnumTypeAsStream(@Nonnull Class<E> enumType) {
-        return Arrays.stream(enumType.getEnumConstants());
+    private static <E extends Enum> Stream<E> expressEnumTypeAsStream(@Nonnull Class<E> enumTypeAsInput) {
+        return Arrays.stream(enumTypeAsInput.getEnumConstants());
     }
 
 }
