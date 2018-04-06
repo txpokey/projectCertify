d43405c0205bdd5c51829b88737c326edc65d4e0
Bob Makowski <tritonpi-git0@yahoo.com>
2018-04-06 17:15:30 -0500

    F:-: generics testing: OracleTutorialQuestion #9. I stopped thinking about refactoring this. have no concrete plan or goal.

--- projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/generic/OracleTutorialQuestion_9.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/generic/OracleTutorialQuestion_9.java
@@ -1,6 +1,12 @@
 package edu.javial.cert.se.sx.type.generic;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.HashMap;
+
 public class OracleTutorialQuestion_9<T> {
+    private static Log log = LogFactory.getLog(OracleTutorialQuestion_9.class);
 
 //    public class Singleton<T> {
 //
@@ -14,10 +20,25 @@ public class OracleTutorialQuestion_9<T> {
 //        private static T instance = null; // COMPILE: can't mix static with generic
 //    }
 
-    public T getInstance() {
+    public T getInstance() { // this code is nonsense
         if (instance == null)
             instance = (T) new OracleTutorialQuestion_9<T>();
         return instance;
     }
     private T instance = null;
 }
+//
+//class OracleTutorialQuestion_9_Refactored<T> {
+//    private static Log log = LogFactory.getLog(OracleTutorialQuestion_9.class);
+////
+////    private static HashMap instance ;
+////
+////    public T getInstance() {
+////        return (null == instance) ? newInstance() : (T) instance.get(0) ;
+////    }
+////    private synchronized T newInstance() {
+////    }
+//    public T getInstance() {
+//        return new T() ;
+//    }
+//}
