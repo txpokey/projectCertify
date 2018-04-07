e53b7064da7424242f4a99f8f6afbd5374c7f955
Bob Makowski <tritonpi-git0@yahoo.com>
2018-04-06 20:26:26 -0500

    T:T:-: generics testing: clean up on Generics tests from Oracle Tutorial.

--- projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/generic/ora/ExploreGenericFromOracleTutorialTest.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/generic/ora/ExploreGenericFromOracleTutorialTest.java
@@ -2,7 +2,9 @@ package edu.javial.cert.se.sx.type.generic.ora;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.Test;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -15,41 +17,68 @@ import java.util.List;
 public class ExploreGenericFromOracleTutorialTest {
     private static Log log = LogFactory.getLog(ExploreGenericFromOracleTutorialTest.class);
 
+    @Test
     public void oraTutorialQuestion_2 () {
-//        public final class Algorithm {
-//            public static <T> T max(T x, T y) {
-//                return x > y ? x : y;  // COMPILE: T is always an Object and '>' terminal is primitive
-//            }
-//        }
+        /*
+        public final class Algorithm {
+            public static <T> T max(T x, T y) {
+                return x > y ? x : y;  // COMPILE: T is always an Object and '>' terminal is primitive
+            }
+        }
+         */
+
         final class Algorithm {
-            public <T extends Comparable<T>> T max(T x, T y) {
+            public <T extends Comparable<T>> T max(T x, T y) { // BUT: no static members 4 inner classes
                 return 0 < x.compareTo(y) ? x : y;
             }
         }
+        Algorithm sut = new Algorithm();
+        Integer ai = BOOYA.max(1,2) ;  // just to show BOOYA has a static involving Type Parameter
+        assert ai.equals(2);
+        String as = sut.max("1","2") ;
+        assert as.equals("2");
+        as = sut.max("2","1") ;
+        assert as.equals("2");
+        Double ad = sut.max( 2.0 , 1.0);
+        assert ad.equals(2.0);
+        log.debug("");
     }
+    @Test
     public void oraTutorialQuestion_10() {
         class Shape { /* ... */ }
         class Circle extends Shape { /* ... */ }
-        class Rectangle extends Shape { /* ... */ }
 
-        class Node<T> { /* ... */ }
+        class Node<T> { T t; /* ... */ }
         Node<Circle> nc = new Node<>();
-//        Node<Shape>  ns = nc;
+        Node<Shape>  ns = new Node<>(); // = nc;  // COMPILE: Node<Shape> !~ Node<Circle>
+        Node<? extends Shape> dummy = new Node<>() ;
 
-        class ReNode<X extends Shape> { } ;
-        ReNode<Shape> rns0 = new ReNode<>() ;
-//        ReNode<Shape> rns = ns ;
+        Class c_nw = dummy.getClass() ;
+        Class c_nc = nc.getClass() ;
+        Class c_ns = ns.getClass() ;
+        assert c_nw.equals(c_nc);
+        assert c_nw.equals(c_ns);
+        dummy = ns ;
+        dummy = nc ;
+
+        log.debug("");
     }
+    @Test(expectedExceptions = ClassCastException.class)
     public void oraTutorialQuestion_11 () {
         class Node<T> implements Comparable<T> {
-            public int compareTo(T obj) { /* ... */ return 0 ;}
+            public int compareTo(@Nonnull T obj) { /* ... */ return 0 ;}
             // ...
         }
         Node<String> node = new Node<>();
-        Comparable<String> comp = node;
-//        Comparable<Integer> comp1 = node;
+        Comparable<String> comp = "1";
+        Comparable compRaw = node ;
+        compRaw = 1 ; // AUTOBOX
+        final int i = compRaw.compareTo(comp); // THROWS : ClassCastException
+        assert false ; // NOTREACHED
+        log.debug("i:> " + i );
     }
 
+    @Test
     public void oraTutorialExerciseWildCardErrorBad() {
 
         class Tuple<T extends Number>  {
@@ -62,6 +91,7 @@ public class ExploreGenericFromOracleTutorialTest {
         class WildcardErrorBad<T extends Number> {
 
 /*
+        .............. original problem .......................
             void swapFirst(List<? extends Number> l1, List<? extends Number> l2) {
                 Number temp = l1.get(0);
                 l1.set(0, l2.get(0)); // expected a CAP#1 extends Number,
@@ -76,7 +106,6 @@ public class ExploreGenericFromOracleTutorialTest {
                 List<T> left = rewrap(l1) ;
                 List<T> right = rewrap(l2) ;
                 swapFirstHelper( left , right ) ;
-                log.debug("");
                 l1 = left;
                 log.debug("");
             }
@@ -84,16 +113,12 @@ public class ExploreGenericFromOracleTutorialTest {
                 List<T> left = rewrap(in.left) ;
                 List<T> right = rewrap(in.right) ;
                 swapFirstHelper( left , right ) ;
-                log.debug("");
                 Tuple candidate = new Tuple(left,right) ;
-                log.debug("");
                 return candidate ;
             }
             Tuple swapFirst0(Tuple in) {
-                swapFirstHelper( in.left , in.right ) ; // FAILS: ArrayStoreException: java.lang.Double
-                log.debug("");
+                swapFirstHelper( in.left , in.right ) ; // THROWS: ArrayStoreException: java.lang.Double
                 Tuple candidate = in ; //  new Tuple(left,right) ;
-                log.debug("");
                 return candidate ;
             }
             private List<T> rewrap( List<? extends Number> in) {
@@ -116,11 +141,16 @@ public class ExploreGenericFromOracleTutorialTest {
 
         List<Integer> li = Arrays.asList(1, 2, 3);
         List<Double>  ld = Arrays.asList(10.10, 20.20, 30.30);
-        SUT.doIt(li, ld);
+        SUT.doIt(li, ld); // TODO : test the swap
 
         List<? extends Number> liT = li ;
         List<? extends Number> ldT = ld ;
-        SUT.doIt(liT, ldT);
+        SUT.doIt(liT, ldT); // TODO : test the swap
 
+    }
+}
+class BOOYA {
+    public static <T extends Comparable<T>> T max(T x, T y) { // BUT: no static members 4 inner classes
+        return 0 < x.compareTo(y) ? x : y;
     }
 }
