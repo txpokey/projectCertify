611095958d8205c44494ece47a70fe11fe9bc271
Bob Makowski <tritonpi-git0@yahoo.com>
2018-04-09 17:31:28 -0500

    T:-: collections : ExampleRequiredOrPermittedAttributeIdiom sci experiment. wanted to try using set theoretics, but found I was writing too much code.

---
+++ projectCertifyJava/src/main/java/edu/javial/cert/se/collections/ExampleRequiredOrPermittedAttributeIdiom.java
@@ -0,0 +1,48 @@
+package edu.javial.cert.se.collections;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ExampleRequiredOrPermittedAttributeIdiom {
+    final static Log log = LogFactory.getLog(ExampleRequiredOrPermittedAttributeIdiom.class);
+
+    static <K, V> boolean validate0(Map<K, V> attrMap, Set<K> requiredAttrs, Set<K>permittedAttrs) {
+        boolean valid = true;
+        Set<K> attrs = attrMap.keySet();
+
+        if (! attrs.containsAll(requiredAttrs)) {
+            Set<K> missing = new HashSet<K>(requiredAttrs);
+            missing.removeAll(attrs);
+            System.out.println("Missing attributes: " + missing);
+            valid = false;
+        }
+        if (! permittedAttrs.containsAll(attrs)) {
+            Set<K> illegal = new HashSet<K>(attrs);
+            illegal.removeAll(permittedAttrs);
+            System.out.println("Illegal attributes: " + illegal);
+            valid = false;
+        }
+        return valid;
+    }
+    static <K, V> boolean validate(Map<K, V> attrMap, Set<K> requiredAttrs, Set<K>permittedAttrs) {
+        boolean valid = true;
+        Set<K> attrs = attrMap.keySet();
+
+        valid = isValidVsAttributes(requiredAttrs, valid, attrs, "Missing attributes: ");
+        valid = isValidVsAttributes(attrs, valid, permittedAttrs, "Illegal attributes: ");
+        return valid;
+    }
+    private static <K> boolean isValidVsAttributes(Set<K> requiredAttrs, boolean valid, Set<K> attrs, String s) {
+        if (!attrs.containsAll(requiredAttrs)) {
+            Set<K> missing = new HashSet<K>(requiredAttrs);
+            missing.removeAll(attrs);
+            log.debug(s + missing);
+            valid = false;
+        }
+        return valid;
+    }
+}
