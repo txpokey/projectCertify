9e8ceddf4e493564528d15ac72e9091b9f9c769d
Bob Makowski <tritonpi-git0@yahoo.com>
2018-04-14 07:09:42 -0500

    T:-: collections : explore null data

---
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/collections/CollectionsExplorationWithNullDataTests.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package edu.javial.cert.se.collections;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+/**
+ * @author mak
+ *
+ */
+@Test
+public class CollectionsExplorationWithNullDataTests {
+	final static Log log = LogFactory.getLog(CollectionsExplorationWithNullDataTests.class);
+
+	public void hashSetSubSetTest() {
+		NavigableSetTester tool = new NavigableSetTester() ;
+		boolean OK = tool.navsetTest() ;
+		assert OK : "hash set testing subsets failed" ;
+	}
+	public void willNullBeAccepted() {
+		willNullBeAcceptedAsHashMapKey() ;
+		willNullBeAcceptedAsHashSetMember() ;
+		willNullBeAcceptedAsArrayListElement() ;
+		willNullBeAcceptedAsTreeMapKey() ;
+		willNullBeAcceptedAsTreeSetMember() ;
+	}
+	private boolean willNullBeAcceptedAsTreeMapKey() {
+		TreeMap<String,Integer> map = new TreeMap<>() ;
+		Object candidate = null;
+		try {
+			candidate = map.put(null,null);
+		} catch (Exception e) {
+			log.debug("null value is not any good to this type:> " + e);
+		}
+		return null == candidate ? false : map.equals(candidate) ;
+	}
+
+	private boolean willNullBeAcceptedAsTreeSetMember() {
+		TreeSet<String> map = new TreeSet<>() ;
+		Object candidate = null;
+		try {
+			candidate = map.add(null);
+		} catch (Exception e) {
+			log.debug("null value is not any good to this type:> " + e);
+		}
+		return null == candidate ? false : map.equals(candidate) ;
+	}
+
+	private boolean willNullBeAcceptedAsArrayListElement() {
+		List<String> map = new ArrayList<>() ;
+		Object candidate = map.add(null) ;
+		return map.equals(candidate);
+	}
+
+	private boolean willNullBeAcceptedAsHashSetMember() {
+		HashSet<String> map = new HashSet<>() ;
+		Object candidate = map.add(null) ;
+		return map.equals(candidate);
+	}
+
+	private boolean willNullBeAcceptedAsHashMapKey() {
+		HashMap<String,Integer> map = new HashMap<>() ;
+		Object candidate = map.put(null, null) ;
+		return map.equals(candidate);
+	}
+
+}
