a3e59a334013e69011f5113bf5229aa3fc708d6a
Bob Makowski <tritonpi-git0@yahoo.com>
2018-04-13 21:59:26 -0500

    T:-: collections : ExampleRequiredOrPermittedAttributeIdiom sci experiment: code clean up refactored the original version, now removing that original

--- projectCertifyJava/src/main/java/edu/javial/cert/se/collections/ExampleRequiredOrPermittedAttributeIdiom.java
+++ projectCertifyJava/src/main/java/edu/javial/cert/se/collections/ExampleRequiredOrPermittedAttributeIdiom.java
@@ -10,24 +10,6 @@ import java.util.Set;
 public class ExampleRequiredOrPermittedAttributeIdiom {
     final static Log log = LogFactory.getLog(ExampleRequiredOrPermittedAttributeIdiom.class);
 
-    static <K, V> boolean validate0(Map<K, V> attrMap, Set<K> requiredAttrs, Set<K>permittedAttrs) {
-        boolean valid = true;
-        Set<K> attrs = attrMap.keySet();
-
-        if (! attrs.containsAll(requiredAttrs)) {
-            Set<K> missing = new HashSet<K>(requiredAttrs);
-            missing.removeAll(attrs);
-            System.out.println("Missing attributes: " + missing);
-            valid = false;
-        }
-        if (! permittedAttrs.containsAll(attrs)) {
-            Set<K> illegal = new HashSet<K>(attrs);
-            illegal.removeAll(permittedAttrs);
-            System.out.println("Illegal attributes: " + illegal);
-            valid = false;
-        }
-        return valid;
-    }
     static <K, V> boolean validate(Map<K, V> attrMap, Set<K> requiredAttrs, Set<K>permittedAttrs) {
         boolean valid = true;
         Set<K> attrs = attrMap.keySet();
