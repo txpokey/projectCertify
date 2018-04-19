6b96c499988a70a3ad24ba02562e3812d8ec423f
Bob Makowski <tritonpi-git0@yahoo.com>
2018-04-19 16:46:00 -0500

    T:T: merged SUT into NavigableSetTester, and deleted CollectionsExplorationTests, which was an orphaned test suite anyway

--- projectCertifyJava/src/test/java/edu/javial/cert/se/collections/NavigableSetTester.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/collections/NavigableSetTester.java
@@ -3,20 +3,21 @@
  */
 package edu.javial.cert.se.collections;
 
+import edu.javial.cert.se.math.PrimeNumber;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.Test;
+
 import java.util.Collection;
 import java.util.NavigableSet;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-import edu.javial.cert.se.math.PrimeNumber;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 /**
  * @author mak
+ *  TODO get NavigableSetTester to work
  */
-public class NavigableSetTester { // TODO get NavigableSetTester to work
+public class NavigableSetTester {
 	private static Log log = LogFactory.getLog(NavigableSetTester.class);
 	private NavigableSet<Integer> ns = null;
     public NavigableSetTester()
@@ -27,7 +28,7 @@ public class NavigableSetTester { // TODO get NavigableSetTester to work
 					PrimeNumber.isPrime(i);
 				}
 				Collection<Integer> primes = PrimeNumber.getPrimes();
-				ns = new TreeSet<>(primes); // TODO threadsafety
+				ns = new TreeSet<>(primes);
 				log.info( ns ) ;
 			} catch (Exception e) {
 				log.fatal("unable to set up tester because of a problem", e);
@@ -55,5 +56,11 @@ public class NavigableSetTester { // TODO get NavigableSetTester to work
 			}
 		} // headset
 		return ret;
+	}
+	@Test
+	public void hashSetSubSetTest() {
+		NavigableSetTester tool = new NavigableSetTester() ;
+		boolean OK = tool.navsetTest() ;
+		assert OK : "hash set testing subsets failed" ;
 	}
 }
