ed54ab82ac8832417c6c0d9cdd645224ac8dec8d
Bob Makowski <tritonpi-git0@yahoo.com>
2018-04-24 23:20:02 -0500

    T:T: ExploreBiFunctionalTypesUsingStackOverflowProblem : got what I wanted. BiFunction working example, but also found pretty elegant way via groupingBy and then reducing the List<T> via a Function...

--- projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/functional/ExploreBiFunctionalTypesUsingStackOverflowProblem.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/sx/type/functional/ExploreBiFunctionalTypesUsingStackOverflowProblem.java
@@ -15,40 +15,42 @@ import static java.util.stream.Collectors.toMap;
 @Test
 public class ExploreBiFunctionalTypesUsingStackOverflowProblem {
     private static Log log = LogFactory.getLog(ExploreBiFunctionalTypesUsingStackOverflowProblem.class);
-// https://stackoverflow.com/questions/26340688/group-by-and-sum-objects-like-in-sql-with-java-lambdas/26346574#26346574
+
+    // https://stackoverflow.com/questions/26340688/group-by-and-sum-objects-like-in-sql-with-java-lambdas/26346574#26346574
     class Foo {
         public int getId() {
             return id;
         }
 
         //            id:int / name;String / targetCost:double / actualCost:double
-        int id ;
-        String name ;
+        int id;
+        String name;
 
         public double getTargetCost() {
             return targetCost;
         }
 
-        double targetCost ;
-        double actualCost ;
-        Foo( int id , String name , double targetCost , double actualCost ) {
-            this.id = id ;
+        double targetCost;
+        double actualCost;
+
+        Foo(int id, String name, double targetCost, double actualCost) {
+            this.id = id;
             this.name = name;
             this.targetCost = targetCost;
             this.actualCost = actualCost;
         }
-        Foo( Foo a , Foo b) {
+
+        Foo(Foo a, Foo b) {
             this(a.id, a.name,
                     a.targetCost + b.targetCost,
                     a.actualCost + b.actualCost);
         }
+
         public String toString() {
-            final String s[] = { this.id + "" , this.name + "" , this.targetCost + "" , this.actualCost + "" };
-            final String j = Stream.of(s).collect(Collectors.joining(","));
-            final String report = "(" + j + ")";
-            return report ;
+            return String.format("(%d,%s,%.2f,%.2f)", id, name, targetCost, actualCost);
         }
     }
+
     final Foo[] data = {
             new Foo(1, "P1", 300, 400),
             new Foo(2, "P2", 600, 400),
@@ -57,129 +59,141 @@ public class ExploreBiFunctionalTypesUsingStackOverflowProblem {
             new Foo(1, "P1", 360, 40),
             new Foo(4, "P4", 320, 200),
             new Foo(4, "P4", 500, 900)
-    } ;
+    };
     final BinaryOperator<Foo> fooConstructorAsBinaryOperator = (a, b) -> new Foo(a, b);
     final BinaryOperator<Foo> fooBinaryOperator = (a, b) -> new Foo(a.id, a.name, a.targetCost + b.targetCost, a.targetCost + b.actualCost);
 
-    public void exploreStreamsCollectedToMapViaConstructorAsBinaryOperatorForMerge0() {
-
-        final Stream<Foo> stream0 = Stream.of(data);
-        Collection<Foo> hv = stream0.collect(toMap( Foo::getId, Function.identity(), fooConstructorAsBinaryOperator
-        )).values();
-        log.debug("");
-    }
     public void exploreStreamsCollectedToMapViaConstructorAsBinaryOperatorForMerge() {
-
-        final Stream<Foo> stream0 = Stream.of(data);
-        Collection<Foo> hv = stream0.collect(toMap( Foo::getId, Function.identity(), fooBinaryOperator )).values();
-        log.debug("");
+        exploreStreamsCollectedToMapViaBinaryOperatorArgumentForMerge(fooConstructorAsBinaryOperator);
     }
-    public void exploreStreamsCollectedByMapsViaBiConsumer2() {
 
+    public void exploreStreamsCollectedToMapViaConstructorAsBinaryOperatorForMerge2() {
+        exploreStreamsCollectedToMapViaBinaryOperatorArgumentForMerge(fooBinaryOperator);
+    }
+    public void exploreStreamsCollectedToMapViaBinaryOperatorArgumentForMerge(BinaryOperator<Foo> mergingOperator) {
+        final Stream<Foo> stream0 = Stream.of(data);
+        Collection<Foo> hv = stream0.collect(toMap(Foo::getId, Function.identity(), mergingOperator)).values();
+        List<Foo> result = new ArrayList<>(hv);
+        log.debug(result);
+    }
+
+    private Function<? super Map.Entry<Integer, List<Foo>>, Foo> reducingFunction = (Map.Entry<Integer, List<Foo>> in)
+            -> {
+        final Foo ret = in.getValue().stream().reduce(fooBinaryOperator).get();
+        return ret;
+    };
+    public void exploreStreamsGroupedIntoMapsButCollectedReducingFunction() {
+        final Stream<Foo> dataStream = Stream.of(data);
+        final Map<Integer, List<Foo>> groupedByIdMap = dataStream.collect(Collectors.groupingBy(Foo::getId));
+        final List<Foo> result = groupedByIdMap.entrySet().stream().collect(Collectors.mapping
+                (reducingFunction, Collectors.toList()));
+        log.debug(result);
+    }
+
+    /********************* SCIENCE EXPERIMENTS *********************************/
+    public void exploreStreamsReducedByBinaryOperator() {
         final Stream<Foo> stream1 = Stream.of(data);
         Optional<Foo> justOneLeft = stream1.reduce(fooConstructorAsBinaryOperator);
-        log.debug("");
+        log.debug(justOneLeft);
     }
-    private BiConsumer<Integer, List<Foo>> reduceBiConsumer(@Nonnull BinaryOperator<Foo> binOp , @Nonnull List<Foo>
-            result ) {
+
+    private BiConsumer<Integer, List<Foo>> reduceBiConsumer(@Nonnull BinaryOperator<Foo> binOp, @Nonnull List<Foo>
+            result) {
         BiConsumer<Integer, List<Foo>> integerListBiConsumer = (key, value) -> {
             Optional<Foo> residueForGivenKey = value.stream().reduce(binOp);
             result.add(residueForGivenKey.get());
         };
-        return integerListBiConsumer ;
+        return integerListBiConsumer;
     }
-    public void exploreStreamsCollectedByMapsViaBiConsumer3() {
-        final Stream<Foo> stream2  = Stream.of(data);
-        Map<Integer,List<Foo>> groupedByIdMap = stream2.collect(Collectors.groupingBy(Foo::getId));
+
+    public void exploreStreamsCollectedByGroupingOverIdsThenReducedViaBiConsumer() {
+        final Stream<Foo> stream2 = Stream.of(data);
+        Map<Integer, List<Foo>> groupedByIdMap = stream2.collect(Collectors.groupingBy(Foo::getId));
         List<Foo> reductionTarget = new ArrayList<>();
         groupedByIdMap.forEach(reduceBiConsumer(fooBinaryOperator, reductionTarget));
         log.debug(reductionTarget);
     }
-    private BiFunction<Object,? super Map<Integer,List<Foo>>,Object> myDummy;
-    private BinaryOperator<Object> moreDummy;
 
-    private void exploreStreamsCollectedByMapsViaBiConsumer4Skaffold() {
-        final Stream<Foo> stream2  = Stream.of(data);
-
-        Map<Integer,List<Foo>> groupedByIdMap = stream2.collect(Collectors.groupingBy(Foo::getId));
-        final Stream<Map<Integer, List<Foo>>> mapStream = Stream.of(groupedByIdMap);
-        List<Foo> dummyList = null ;
-        BiFunction<List<Foo>, ? super Map<Integer, List<Foo>>, List<Foo>> myDummy = null ;
-        BinaryOperator<List<Foo>> moreDummy = null ;
-        final List<Foo> reduceResult = mapStream.reduce(dummyList, myDummy, moreDummy) ;
-        log.debug(reduceResult);
-    }
     final private BinaryOperator<List<Foo>> combineListsOfFooBiOperator = (foo0, foo1) -> {
         List<Foo> ret = new ArrayList<>();
-        ret.addAll(foo0) ;
-        ret.addAll(foo1) ;
-        return ret ;
-    }  ;
-    public void exploreStreamsCollectedByMapsViaBiConsumer4() {
-        final Stream<Foo> stream2  = Stream.of(data);
+        ret.addAll(foo0);
+        ret.addAll(foo1);
+        return ret;
+    };
 
-        Map<Integer,List<Foo>> groupedByIdMap = stream2.collect(Collectors.groupingBy(Foo::getId));
+    public void exploreStreamsCollectedByGroupingOverIdsThenReducedViaBiConsumerInnerClass() {
+        final Stream<Foo> stream2 = Stream.of(data);
+
+        Map<Integer, List<Foo>> groupedByIdMap = stream2.collect(Collectors.groupingBy(Foo::getId));
         final Stream<Map<Integer, List<Foo>>> mapStream = Stream.of(groupedByIdMap);
-        BiFunction<List<Foo>, ? super Map<Integer, List<Foo>>, List<Foo>> reducingBiFunction = (List<Foo> result, Map<Integer, List<Foo>> in ) -> {
-            List<Foo> ret = result ;
+        BiFunction<List<Foo>, ? super Map<Integer, List<Foo>>, List<Foo>> reducingBiFunction = (List<Foo> result, Map<Integer, List<Foo>> in) -> {
+            List<Foo> ret = result;
             in.entrySet().forEach(
                     new Consumer<Map.Entry<Integer, List<Foo>>>() {
                         @Override
                         public void accept(Map.Entry<Integer, List<Foo>> integerListEntry) {
                             final Optional<Foo> fooOptional = integerListEntry.getValue().stream().reduce(fooBinaryOperator);
-                            ret.add( fooOptional.get() );
+                            ret.add(fooOptional.get());
                         }
                     }
             );
-            return ret ;
+            return ret;
         };
         List<Foo> reductionTarget = new ArrayList<>();
-        final List<Foo> reduceResult = mapStream.reduce(reductionTarget, reducingBiFunction, combineListsOfFooBiOperator) ;
+        final List<Foo> reduceResult = mapStream.reduce(reductionTarget, reducingBiFunction, combineListsOfFooBiOperator);
         log.debug(reduceResult);
     }
-    public void exploreStreamsCollectedByMapsViaBiConsumer4L() {
-        final Stream<Foo> stream2  = Stream.of(data);
 
-        Map<Integer,List<Foo>> groupedByIdMap = stream2.collect(Collectors.groupingBy(Foo::getId));
+    public void exploreStreamsCollectedByGroupingOverIdsThenReducedViaBiConsumerInnerClassConertedToLambda() {
+        final Stream<Foo> stream2 = Stream.of(data);
+
+        Map<Integer, List<Foo>> groupedByIdMap = stream2.collect(Collectors.groupingBy(Foo::getId));
         final Stream<Map<Integer, List<Foo>>> mapStream = Stream.of(groupedByIdMap);
         BiFunction<List<Foo>, ? super Map<Integer, List<Foo>>, List<Foo>> reducingBiFunction = (List<Foo> result,
-                                                                                          Map<Integer, List<Foo>> in ) -> {
-            List<Foo> ret = result ;
+                                                                                                Map<Integer, List<Foo>> in) -> {
+            List<Foo> ret = result;
             in.entrySet().forEach(
                     integerListEntry -> {
                         final Optional<Foo> fooOptional = integerListEntry.getValue().stream().reduce(fooBinaryOperator);
-                        ret.add( fooOptional.get() );
+                        ret.add(fooOptional.get());
                     }
             );
-            return ret ;
+            return ret;
         };
         List<Foo> reductionTarget = new ArrayList<>();
-        final List<Foo> reduceResult = mapStream.reduce(reductionTarget, reducingBiFunction, combineListsOfFooBiOperator) ;
+        final List<Foo> reduceResult = mapStream.reduce(reductionTarget, reducingBiFunction, combineListsOfFooBiOperator);
         log.debug(reduceResult);
     }
-    public void exploreStreamsCollectedByMapsViaBiConsumer5() {
-        final Stream<Foo> stream2  = Stream.of(data);
 
-        Map<Integer,List<Foo>> groupedByIdMap = stream2.collect(Collectors.groupingBy(Foo::getId));
+    private BiFunction<List<Foo>, ? super Map<Integer, List<Foo>>, List<Foo>> reducingBiFunction =
+            (List<Foo> result, Map<Integer, List<Foo>> in) -> {
+                in.entrySet().forEach(
+                        getEntryConsumer(result)
+                );
+                return result;
+            };
+
+    public void exploreStreamsCollectedByGroupingOverIdsThenReducedViaBiFunction() {
+        final Stream<Foo> stream2 = Stream.of(data);
+
+        Map<Integer, List<Foo>> groupedByIdMap = stream2.collect(Collectors.groupingBy(Foo::getId));
         final Stream<Map<Integer, List<Foo>>> mapStream = Stream.of(groupedByIdMap);
-        BiFunction<List<Foo>, ? super Map<Integer, List<Foo>>, List<Foo>> reducingBiFunction = (List<Foo> result, Map<Integer, List<Foo>> in ) -> {
-            List<Foo> ret = result ;
-            in.entrySet().forEach(
-                    integerListEntry -> {
-                        List<Foo> tmp = integerListEntry.getValue();
-                        Foo patsy = tmp.get(0);
-                        Foo identity = new Foo(patsy.id, patsy.name, 0, 0);
-                        final Foo foo = integerListEntry.getValue().stream().reduce(
-                                identity,
-                                fooBinaryOperator);
-                        ret.add(foo);
-                    }
-            );
-            return ret ;
-        };
+
         List<Foo> reductionTarget = new ArrayList<>();
-        final List<Foo> reduceResult = mapStream.reduce(reductionTarget, reducingBiFunction, combineListsOfFooBiOperator) ;
+        final List<Foo> reduceResult = mapStream.reduce(reductionTarget, reducingBiFunction, combineListsOfFooBiOperator);
         log.debug(reduceResult);
+    }
+
+    private Consumer<Map.Entry<Integer, List<Foo>>> getEntryConsumer(List<Foo> ret) {
+        return integerListEntry -> {
+            List<Foo> tmp = integerListEntry.getValue();
+            Foo patsy = tmp.get(0);
+            Foo identity = new Foo(patsy.id, patsy.name, 0, 0);
+            final Foo foo = integerListEntry.getValue().stream().reduce(
+                    identity,
+                    fooBinaryOperator);
+            ret.add(foo);
+        };
     }
 }
 
