d78fbabf68b84b20259869fd4972e74d528ad81a
Bob Makowski <tritonpi-git0@yahoo.com>
2018-05-04 11:22:21 -0500

    T:T: ExploreMetaDataForOriginalFileObjectsByTest : java.io.File discovery : added HashMap approach to walking Path.getName()

--- projectCertifyJava/src/test/java/edu/javial/cert/se/corelib/io/ExploreMetaDataForOriginalFileObjectsByTest.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/corelib/io/ExploreMetaDataForOriginalFileObjectsByTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.testng.annotations.Test;
 
+import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,25 +13,33 @@ import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Optional;
 
 @Test
 public class ExploreMetaDataForOriginalFileObjectsByTest {
     private static Log log = LogFactory.getLog(ExploreMetaDataForOriginalFileObjectsByTest.class);
-    private final File devdir = new File( "/dev" ) ;
-    private final File devnull = new File( "/dev/null" ) ;
-    private final File devtty = new File( "/dev/tty" ) ;
-    private final File fakeout = new File( "/dev/fakeout" ) ;
-    private final File fakeoutDir = new File( "/fakeout" ) ;
-    private final File fakeoutDir2 = new File( "/fakeout/" ) ;
-    private final File ideaLink = new File( "/home/mak/ln/idea" ) ;
-    private final File idea = new File("/opt/com/jetbrains/idea/current/bin/idea.sh" ) ;
-    private final File[] filesForAttributes = {
+    private final static File devdir = new File( "/dev" ) ;
+    private final static File devnull = new File( "/dev/null" ) ;
+    private final static File devtty = new File( "/dev/tty" ) ;
+    private final static File fakeout = new File( "/dev/fakeout" ) ;
+    private final static File fakeoutDir = new File( "/fakeout" ) ;
+    private final static File fakeoutDir2 = new File( "/fakeoutSlash/" ) ;
+    private final static File ideaLink = new File( "/home/mak/ln/idea" ) ;
+    private final static File idea = new File("/opt/com/jetbrains/idea/current/bin/idea.sh" ) ;
+    private final static File[] filesForAttributes = {
             devdir, devnull, idea, ideaLink
     };
-    private final File[] filesForClassification = {
-            devtty, devdir, fakeout, fakeoutDir, fakeoutDir2, devnull, idea, ideaLink
+    private final static File[] filesForClassification = {
+            ideaLink, devtty, devdir, fakeout, fakeoutDir, fakeoutDir2, devnull, idea, ideaLink
     };
+
+    public static File[] getFilesForAttributedsTesting() {
+        return filesForAttributes ;
+    }
+    public static File[] getFilesForForClassificationTesting() {
+        return filesForClassification ;
+    }
 
     public void exploreLinkOption(){
         LinkOption[] options = LinkOption.values();
@@ -47,52 +56,58 @@ public class ExploreMetaDataForOriginalFileObjectsByTest {
 
         log.debug("");
     }
-    private void exploreFileClassificationInspectors(File subjectFile ) {
-        checkIfExists( subjectFile ) ;
-        checkIfRegularFile( subjectFile ) ;
-        checkIfOther( subjectFile ) ;
-        checkIfOrdinaryFile( subjectFile ) ;
-        checkIfDirectory( subjectFile ) ;
+    private void exploreFileClassificationInspectors(@Nonnull File subjectFile ) {
+        final Optional<BasicFileAttributes> attributes = getBasicFileAttributes(subjectFile) ;
+        final HashMap<String, Boolean> report = new HashMap<>() ;
+        report.put( "IfExists", checkIfExists( subjectFile , attributes) ) ;
+        report.put( "IfRegularFile", checkIfRegularFile( subjectFile , attributes) ) ;
+        report.put( "IfOrdinaryFile", checkIfOrdinaryFile( subjectFile , attributes) ) ;
+        report.put( "IfDirectory", checkIfDirectory( subjectFile , attributes) ) ;
+        report.put( "IfOther", checkIfOther( subjectFile , attributes) ) ;
+        report.put( "IfSymbolicLink", checkIfSymbolicLink( subjectFile , attributes) ) ;
+
+        log.debug( subjectFile + ":> " + report );
     }
-    private boolean checkIfExists(File subjectFile ) {
+    private boolean checkIfExists(@Nonnull File subjectFile , final @Nonnull Optional<BasicFileAttributes> attributes
+    ) {
         boolean candidate = subjectFile.exists() ;
-        log.debug( subjectFile + ":exists()> " + candidate );
         return candidate;
     }
-    private boolean checkIfOrdinaryFile(File subjectFile ) {
+    private boolean checkIfOrdinaryFile(@Nonnull File subjectFile, final @Nonnull Optional<BasicFileAttributes> attributes ) {
         boolean candidate = subjectFile.isFile() ;
-        log.debug( subjectFile + ":isFile()> " + candidate );
         return candidate;
     }
-    private boolean checkIfDirectory(File subjectFile ) {
+    private boolean checkIfDirectory(@Nonnull File subjectFile , final @Nonnull Optional<BasicFileAttributes> attributes) {
         boolean candidate = subjectFile.isDirectory() ;
-        log.debug( subjectFile + ":isDirectory()> " + candidate );
         return candidate;
     }
-    private void exploreFileLastModificationTime(File subjectFile ) {
+   private boolean checkIfOther(@Nonnull File subjectFile , final @Nonnull Optional<BasicFileAttributes> attributes) {
+        boolean isOther = attributes.isPresent() ? attributes.get().isOther() : false ;
+        return isOther;
+    }
+    private boolean checkIfRegularFile(@Nonnull File subjectFile , final @Nonnull Optional<BasicFileAttributes> attributes) {
+        boolean isRegularFile = attributes.isPresent() ? attributes.get().isRegularFile() : false ;
+        return isRegularFile;
+    }
+    private boolean checkIfSymbolicLink(@Nonnull File subjectFile , final @Nonnull Optional<BasicFileAttributes>
+            attributes) {
+//        boolean doubleCheck = attributes.get().isSymbolicLink() ;
+        boolean isSymbolicLink = attributes.isPresent() ? attributes.get().isSymbolicLink() : false ;
+        return isSymbolicLink;
+    }
+    private void exploreFileLastModificationTime(@Nonnull File subjectFile ) {
         long lastModified = subjectFile.lastModified() ;
         Instant instant = Instant.ofEpochMilli(lastModified);
         log.debug( subjectFile + ":lastModified> " + instant );
     }
-    private void exploreFileLastAccessTime(File subjectFile ) {
+    private void exploreFileLastAccessTime(@Nonnull File subjectFile ) {
         Optional<BasicFileAttributes> attributes = getBasicFileAttributes(subjectFile);
         long lastAccessTime = attributes.isPresent() ? attributes.get().lastAccessTime().toMillis() : 0 ;
         Instant instant = Instant.ofEpochMilli(lastAccessTime);
         log.debug( subjectFile + ":lastAccessTime> " + instant );
     }
-    private boolean checkIfOther(File subjectFile ) {
-        Optional<BasicFileAttributes> attributes = getBasicFileAttributes(subjectFile);
-        boolean isOther = attributes.isPresent() ? attributes.get().isOther() : false ;
-        log.debug( subjectFile + ":isOther> " + isOther );
-        return isOther;
-    }
-    private boolean checkIfRegularFile(File subjectFile ) {
-        Optional<BasicFileAttributes> attributes = getBasicFileAttributes(subjectFile);
-        boolean isRegularFile = attributes.isPresent() ? attributes.get().isRegularFile() : false ;
-        log.debug( subjectFile + ":isRegularFile> " + isRegularFile );
-        return isRegularFile;
-    }
-    private Optional<BasicFileAttributes> getBasicFileAttributes(File subjectFile ) {
+
+    private Optional<BasicFileAttributes> getBasicFileAttributes(@Nonnull File subjectFile ) {
         Path path = subjectFile.toPath() ;
         BasicFileAttributes attrs = null ;
         try {
