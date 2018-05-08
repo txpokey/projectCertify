73164abd03d66825a253968744905298c470760e
Bob Makowski <tritonpi-git0@yahoo.com>
2018-05-07 20:24:34 -0500

    T:T: CollectionsExplorationWithNullDataTests : fixed some silly logic and made a quick report out of the question as to accepting nulls in Collections...

--- projectCertifyJava/src/test/java/edu/javial/cert/se/corelib/collections/CollectionsExplorationWithNullDataTests.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/corelib/collections/CollectionsExplorationWithNullDataTests.java
@@ -1,12 +1,11 @@
-/**
- * 
- */
+
 package edu.javial.cert.se.corelib.collections;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.testng.annotations.Test;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 /**
@@ -22,51 +21,65 @@ public class CollectionsExplorationWithNullDataTests {
 		boolean OK = tool.navsetTest() ;
 		assert OK : "hash set testing subsets failed" ;
 	}
-	public void willNullBeAccepted() {
-		willNullBeAcceptedAsHashMapKey() ;
-		willNullBeAcceptedAsHashSetMember() ;
-		willNullBeAcceptedAsArrayListElement() ;
-		willNullBeAcceptedAsTreeMapKey() ;
-		willNullBeAcceptedAsTreeSetMember() ;
+	public void wasNullBeAccepted() {
+		HashMap<String,Boolean> results = new HashMap<>();
+		results.put("wasNullBeAcceptedAsHashMapKey" , wasNullBeAcceptedAsHashMapKey() ) ;
+		results.put("wasNullBeAcceptedAsHashSetMember" , wasNullBeAcceptedAsHashSetMember() ) ;
+		results.put("wasNullBeAcceptedAsArrayListElement" , wasNullBeAcceptedAsArrayListElement() ) ;
+		results.put("wasNullBeAcceptedAsTreeMapKey" , wasNullBeAcceptedAsTreeMapKey() ) ;
+		results.put("wasNullBeAcceptedAsTreeSetMember" , wasNullBeAcceptedAsTreeSetMember() ) ;
+		log.debug(results);
 	}
-	private boolean willNullBeAcceptedAsTreeMapKey() {
+//2018-05-07|20:21:55 CollectionsExplorationWithNullDataTests.wasNullBeAcceptedAsTreeMapKey.41 [DEBUG] null value is not any good to this type:> java.lang.NullPointerException
+//2018-05-07|20:21:55 CollectionsExplorationWithNullDataTests.wasNullBeAcceptedAsTreeSetMember.54 [DEBUG] null value is not any good to this type:> java.lang.NullPointerException
+//2018-05-07|20:21:55 CollectionsExplorationWithNullDataTests.wasNullBeAccepted.31 [DEBUG] {wasNullBeAcceptedAsTreeSetMember=false, wasNullBeAcceptedAsArrayListElement=true, wasNullBeAcceptedAsHashMapKey=true, wasNullBeAcceptedAsHashSetMember=true, wasNullBeAcceptedAsTreeMapKey=false}
+
+	private boolean wasNullBeAcceptedAsTreeMapKey() {
 		TreeMap<String,Integer> map = new TreeMap<>() ;
 		Object candidate = null;
+		boolean ret = false ;
 		try {
 			candidate = map.put(null,null);
+			ret = null == candidate;
 		} catch (Exception e) {
 			log.debug("null value is not any good to this type:> " + e);
 		}
-		return null == candidate ? false : map.equals(candidate) ;
+		return ret ;
 	}
 
-	private boolean willNullBeAcceptedAsTreeSetMember() {
+	private boolean wasNullBeAcceptedAsTreeSetMember() {
 		TreeSet<String> map = new TreeSet<>() ;
 		Object candidate = null;
+		boolean ret = false ;
 		try {
 			candidate = map.add(null);
+			ret = null == candidate;
 		} catch (Exception e) {
 			log.debug("null value is not any good to this type:> " + e);
 		}
-		return null == candidate ? false : map.equals(candidate) ;
+		return ret ;
 	}
 
-	private boolean willNullBeAcceptedAsArrayListElement() {
-		List<String> map = new ArrayList<>() ;
-		Object candidate = map.add(null) ;
-		return map.equals(candidate);
+	private boolean wasNullBeAcceptedAsArrayListElement() {
+		List<String> list = new ArrayList<>() ;
+		Boolean candidate = list.add(null) ;
+		return candidate;
 	}
 
-	private boolean willNullBeAcceptedAsHashSetMember() {
+	private boolean wasNullBeAcceptedAsHashSetMember() {
 		HashSet<String> map = new HashSet<>() ;
-		Object candidate = map.add(null) ;
-		return map.equals(candidate);
+		Boolean candidate = map.add(null) ;
+		return candidate;
 	}
 
-	private boolean willNullBeAcceptedAsHashMapKey() {
-		HashMap<String,Integer> map = new HashMap<>() ;
+	private boolean wasNullBeAcceptedAsHashMapKey() {
+		HashMap<String,LocalDateTime> map = new HashMap<>() ;
+		LocalDateTime now = LocalDateTime.now();
 		Object candidate = map.put(null, null) ;
-		return map.equals(candidate);
+		boolean isNull = candidate == null ;
+		boolean isTime = now.equals(candidate = map.put(null, now));
+		isTime = now.equals(candidate = map.put(null, now));
+		return isNull && isTime ;
 	}
 
 }
