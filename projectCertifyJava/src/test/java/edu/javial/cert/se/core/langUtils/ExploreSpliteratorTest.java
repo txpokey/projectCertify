acae6b1df1e07e4c93da24dc8b8e2a740709aa11
Bob Makowski <tritonpi-git0@yahoo.com>
2018-05-17 13:35:28 -0500

    T:T: walking code : clean ups and marking todos

---
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/core/langUtils/ExploreSpliteratorTest.java
@@ -0,0 +1,16 @@
+package edu.javial.cert.se.core.langUtils;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.Test;
+
+// TODO : need code here
+
+/**
+ * smoke test to understand what a Spliterator does :)
+ */
+@Test
+public class ExploreSpliteratorTest {
+    private static Log log = LogFactory.getLog(ExploreSpliteratorTest.class);
+
+}
