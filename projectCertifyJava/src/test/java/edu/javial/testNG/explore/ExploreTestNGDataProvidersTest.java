89199e7ee5b8c7a776700b7bff7cb59cd7734375
Bob Makowski <tritonpi-git0@yahoo.com>
2018-05-17 13:34:51 -0500

    T:T: ExploreTestNGDataProvidersTest : proved injector method ok non-static

---
+++ projectCertifyJava/src/test/java/edu/javial/testNG/explore/ExploreTestNGDataProvidersTest.java
@@ -0,0 +1,63 @@
+package edu.javial.testNG.explore;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+/**
+ * DataProviders smoke test ala example found in
+ *
+ * @see <a href="http://practicalunittesting.com/">
+ *     Practical Unit Testingwith TestNG and Mockito
+ *      <i>Tomek Kaczanowski</i></a>
+ */
+@Test
+public class ExploreTestNGDataProvidersTest {
+    @DataProvider
+    /**
+     * notice that the book has this as a static method, but that's not necessary
+     */
+    private final Object[][] getMoney() {
+        return new Object[][]{
+                {10, "USD"},
+                {20, "EUR"}
+        };
+    }
+
+    class Money {
+        private final int amount;
+        private final String currency;
+
+        public Money(int amount, String currency) {
+            this.amount = amount;
+            this.currency = currency;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public String getCurrency() {
+            return currency;
+        }
+
+        public boolean equals(Object o) {
+            if (o instanceof Money) {
+                Money money = (Money) o;
+                return money.getCurrency().equals(getCurrency())
+                        && getAmount() == money.getAmount();
+            }
+            return false;
+        }
+    }
+
+    @Test(dataProvider = "getMoney")
+    public void constructorShouldSetAmountAndCurrency(
+            int amount, String currency) {
+        Money money = new Money(amount, currency);
+        assertEquals(money.getAmount(), amount);
+        assertEquals(money.getCurrency(), currency);
+    }
+}
+
