4aaf3610076d3ac8a3fca486fe406aa735a36bed
Bob Makowski <tritonpi-git0@yahoo.com>
2018-12-02 16:31:39 -0600

    T:T: spring-boot 2.1.1 upgrade

--- projectCertifySpring/dataRest/src/main/groovy/sci/category/certify/service/config/PrimesContentServiceConfig.groovy
+++ projectCertifySpring/dataRest/src/main/groovy/sci/category/certify/service/config/PrimesContentServiceConfig.groovy
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import sci.category.certify.service.PrimesContentServiceUsingH2
-import sci.category.certify.service.bootstrap.BootstrapDataService
 
 @Configuration
 
