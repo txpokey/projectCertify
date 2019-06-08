1fc4adcec4994f21b1907c2012bbd094acd5816c
Bob Makowski <tritonpi-git0@yahoo.com>
2019-06-08 13:52:01 -0500

    -:F: rebooting: sci.rx project: some optimization of the gradle files. removed a contract on the repos. spring tests fail

--- projectCertifySpring/sci.rx/primes.common/src/main/groovy/sci/category/certify/repo/rx/PrimesRxRepoContract.groovy
+++
@@ -1,9 +0,0 @@
-package sci.category.certify.repo.rx
-
-import sci.category.certify.repo.PrimesContentRepo
-
-interface PrimesRxRepoContract  extends PrimesContentRepo  {
-
-//    Mono<Primes> save( Primes p )
-
-}
