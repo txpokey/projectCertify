c52c1bfc392f9a1f0173089ebfc93fc3c8220447
Bob Makowski <tritonpi-git0@yahoo.com>
2019-06-18 10:17:12 -0500

    T:T: primes.common: trivial to add species member to Primes entity

--- projectCertifySpring/sci.rx/primes.common/src/main/groovy/sci/category/certify/domain/Primes.groovy
+++ projectCertifySpring/sci.rx/primes.common/src/main/groovy/sci/category/certify/domain/Primes.groovy
@@ -1,12 +1,12 @@
 package sci.category.certify.domain
 
+import org.springframework.lang.NonNull
+
 import javax.persistence.Entity
-
-//import javax.persistence.Entity
-//import org.springframework.data.mongodb.core.mapping.Document
-
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
+
+//import org.springframework.data.mongodb.core.mapping.Document
 import javax.persistence.Id
 
 @Entity
@@ -18,4 +18,11 @@ class Primes{
 
     Integer prime
     Boolean truth
+    String species
+    String toString() { "[${prime},${species}]"}
+    static Primes of(@NonNull Integer p, String species = "TBD") {
+        def primeAsMap = [ prime: p , species: species ]
+        def primeRx = new Primes(primeAsMap)
+        primeRx
+    }
 }
