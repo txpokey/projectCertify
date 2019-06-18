cea99cce1e5995293d108ec738774efc75caf57f
Bob Makowski <tritonpi-git0@yahoo.com>
2019-06-18 14:32:46 -0500

    T:T: sci.repos.webflux.postgres : PrimesWebfluxRepoPostgreSql repository : DI success

--- projectCertifySpring/sci.rx/sci.repos.webflux.postgresql/src/test/groovy/sci/category/certify/service/config/AdhocPostgreSqlConfigTest.groovy
+++ projectCertifySpring/sci.rx/sci.repos.webflux.postgresql/src/test/groovy/sci/category/certify/service/config/AdhocPostgreSqlConfigTest.groovy
@@ -1,17 +1,19 @@
 package sci.category.certify.service.config
 
 import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests
 import org.testng.annotations.Test
+import sci.category.certify.repo.PrimesWebfluxRepoPostgreSql
 
 @Test
 @Slf4j
 @SpringBootTest
 class AdhocPostgreSqlConfigTest extends AbstractTestNGSpringContextTests {
 
-//    @Autowired
-//    PrimesWebfluxRepoPostgreSql repository
+    @Autowired
+    PrimesWebfluxRepoPostgreSql repository
 
     void sanityCheck() {
         assert true
