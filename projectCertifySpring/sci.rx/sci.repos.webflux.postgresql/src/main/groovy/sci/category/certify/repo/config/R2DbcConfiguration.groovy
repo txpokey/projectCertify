48357bdd72c8b2a85e33d68e1bbc42c485908423
Bob Makowski <tritonpi-git0@yahoo.com>
2019-06-22 18:49:49 -0500

    T:T: Josh Long using postgress : repo/config/R2DbcConfiguration using constructor DI with connectionFactory

---
+++ projectCertifySpring/sci.rx/sci.repos.webflux.postgresql/src/main/groovy/sci/category/certify/repo/config/R2DbcConfiguration.groovy
@@ -0,0 +1,21 @@
+package sci.category.certify.repo.config
+
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+
+@Configuration
+@EnableR2dbcRepositories
+class R2DbcConfiguration extends AbstractR2dbcConfiguration {
+
+    private final ConnectionFactory connectionFactory
+
+    R2DbcConfiguration( ConnectionFactory connectionFactory ) {
+        this.connectionFactory = connectionFactory
+    }
+    @Override
+    ConnectionFactory connectionFactory() {
+        this.connectionFactory
+    }
+}
