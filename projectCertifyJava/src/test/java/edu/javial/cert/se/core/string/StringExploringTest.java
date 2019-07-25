ac28301db623dca74bb22b452a64ac73a2c95afb
Bob Makowski <tritonpi-git0@yahoo.com>
2019-07-25 12:18:17 -0500

    T:T: projectCertifyJava : nice proof on the differences between creating String

--- projectCertifyJava/src/test/java/edu/javial/cert/se/core/string/StringExploringTest.java
+++ projectCertifyJava/src/test/java/edu/javial/cert/se/core/string/StringExploringTest.java
@@ -8,12 +8,24 @@ public class StringExploringTest  {
         final String UPPERCASE_REGEX = "[A-Z]" ;
         return s.substring(0,1).matches(UPPERCASE_REGEX);
     }
-    @Test
     public void testIsCapitalized() throws Exception {
         assert isCapitalized("A") ;
         assert false == isCapitalized("a") ;
         assert false == isCapitalized("1") ;
         assert false == isCapitalized("~") ;
         assert false == isCapitalized(" ") ;
+    }
+
+    final String EXAMPLE_STRING = "Example on Heap but is it Reused?" ;
+    String ExampleOnHeapAndReUsed = EXAMPLE_STRING ;
+
+    String ExampleWithConstructor = new String(EXAMPLE_STRING);
+
+    public void exploreReUseFromHeap() {
+        String onStack = EXAMPLE_STRING ;
+        assert onStack == ExampleOnHeapAndReUsed ;
+        String newOnStack = new String(EXAMPLE_STRING) ;
+        assert onStack != newOnStack ;
+        assert ExampleWithConstructor != newOnStack ;
     }
 }
