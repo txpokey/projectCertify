8d31f9b73507f101c12b9ab45971cc5bb447268e
Bob Makowski <tritonpi-git0@yahoo.com>
2020-04-12 12:29:27 -0500

    micronaut: forced cp while triage swagger problems

--- jvmFrameworks/micronautSkills/sci.micronaut/hello/hello-gorm/src/main/groovy/hello/gorm/HelloGormApplication.groovy
+++ jvmFrameworks/micronautSkills/sci.micronaut/hello/hello-gorm/src/main/groovy/hello/gorm/HelloGormApplication.groovy
@@ -2,7 +2,20 @@ package hello.gorm
 
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "hello-gorm",
+                version = "0.0",
+                description = "My API",
+                license = @License(name = "Apache 2.0", url = "http://foo.bar"),
+                contact = @Contact(url = "http://gigantic-server.com", name = "Fred", email = "Fred@gigagantic-server.com")
+        )
+)
 @CompileStatic
 class HelloGormApplication{
     static void main(String[] args) {
