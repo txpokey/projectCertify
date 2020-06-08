9763c45ea49c80a33b546934a234baa364457ad9
Bob Makowski <tritonpi-git0@yahoo.com>
2020-06-08 13:14:27 -0500

    T:T: added DI for currencyUrl

--- jvmFrameworks/springSkills/sci.spring/sci.restTemplate.maps/src/main/java/sci/spring/rest/template/maps/cfg/RestTemplateCfg.java
+++ jvmFrameworks/springSkills/sci.spring/sci.restTemplate.maps/src/main/java/sci/spring/rest/template/maps/cfg/RestTemplateCfg.java
@@ -1,14 +1,28 @@
 package sci.spring.rest.template.maps.cfg;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "app")
 public class RestTemplateCfg {
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate;
     }
+
+    private String currencyUrl;
+    @Bean(name = "currencyUrl")
+    public String getCurrencyUrl() {
+        return currencyUrl;
+    }
+    public void setCurrencyUrl(String currencyUrl) {
+        this.currencyUrl = currencyUrl;
+    }
+
 }
