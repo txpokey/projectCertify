a63e7b67d8ab3788bd41901568551453c5035d0c
Bob Makowski <tritonpi-git0@yahoo.com>
2020-06-08 13:12:58 -0500

    T:T: added DI for currencyUrl

--- jvmFrameworks/springSkills/sci.spring/sci.restTemplate.maps/src/test/java/sci/spring/rest/template/maps/cfg/RestTemplateCfgTest.java
+++ jvmFrameworks/springSkills/sci.spring/sci.restTemplate.maps/src/test/java/sci/spring/rest/template/maps/cfg/RestTemplateCfgTest.java
@@ -2,11 +2,14 @@ package sci.spring.rest.template.maps.cfg;
 
 import groovy.util.logging.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.web.client.RestTemplate;
 import org.testng.annotations.Test;
+import sci.spring.rest.template.maps.service.FixerIoCurrencyService;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -19,11 +22,18 @@ public class RestTemplateCfgTest extends AbstractTestNGSpringContextTests {
     private RestTemplate restTemplate;
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    protected FixerIoCurrencyService service ;
+    @Autowired
+    @Qualifier("currencyUrl")
+    private String currencyUrl;
 
     public void sanityCheck() {
         logger.debug("PING! from sanity check");
         assert null != restTemplate;
         assert null != testRestTemplate;
+        assert null != service;
+        assert null != currencyUrl;
     }
     public RestTemplate getRestTemplate() {
         return restTemplate;
