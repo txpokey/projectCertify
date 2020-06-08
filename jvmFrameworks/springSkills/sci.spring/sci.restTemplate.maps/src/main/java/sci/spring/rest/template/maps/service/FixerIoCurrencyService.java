7cd51172439eb0e4a153d71071bfcf75320b1859
Bob Makowski <tritonpi-git0@yahoo.com>
2020-06-08 13:12:10 -0500

    T:T: fixed FixerIoCurrencyService as it was not doing anything of use

--- jvmFrameworks/springSkills/sci.spring/sci.restTemplate.maps/src/main/java/sci/spring/rest/template/maps/service/FixerIoCurrencyService.java
+++ jvmFrameworks/springSkills/sci.spring/sci.restTemplate.maps/src/main/java/sci/spring/rest/template/maps/service/FixerIoCurrencyService.java
@@ -7,17 +7,19 @@ import sci.spring.rest.template.maps.domain.Currency;
 
 @Service
 public class FixerIoCurrencyService implements CurrencyServiceContract {
+
     @Override
     public Currency getCurrency(String currencyCode) {
-//        Currency currency = restTemplate.getForObject("http://api.fixer.io/latest?base={currencyCode}", Currency.class, currencyCode);
-        Currency currency = null ;
+        Currency currency = getRestTemplate().getForObject(buildUrl, Currency.class);
         return currency;
+    }
+
+    public RestTemplate getRestTemplate() {
+        return restTemplate;
     }
 
     @Autowired
     private RestTemplate restTemplate;
-
-    public void setRestTemplate(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+    private final static String buildUrl = "http://data.fixer" +
+            ".io/api/latest?access_key=3e71e8caaa61c017d627f66461436b5f&symbols=USD";
 }
