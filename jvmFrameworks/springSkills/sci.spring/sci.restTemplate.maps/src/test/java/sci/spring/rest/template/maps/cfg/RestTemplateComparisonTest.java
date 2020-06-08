ca3962a183dcb740b2b5335c9a71e135a26b30c3
Bob Makowski <tritonpi-git0@yahoo.com>
2020-06-08 13:10:50 -0500

    T:T: added test using TestRestTemplate, comparison

--- jvmFrameworks/springSkills/sci.spring/sci.restTemplate.maps/src/test/java/sci/spring/rest/template/maps/cfg/RestTemplateComparisonTest.java
+++ jvmFrameworks/springSkills/sci.spring/sci.restTemplate.maps/src/test/java/sci/spring/rest/template/maps/cfg/RestTemplateComparisonTest.java
@@ -13,6 +13,7 @@ import sci.spring.rest.template.maps.domain.Currency;
 public class RestTemplateComparisonTest extends RestTemplateCfgTest {
     private final static String buildUrl = "http://data.fixer" +
             ".io/api/latest?access_key=3e71e8caaa61c017d627f66461436b5f&symbols=USD";
+
     public void testRestTemplate0() {
         sanityCheck();
         String json = getTestRestTemplate().getForObject(buildUrl, String.class);
@@ -29,9 +30,19 @@ public class RestTemplateComparisonTest extends RestTemplateCfgTest {
         }
         Assert.notNull(map);
     }
+
     public void testRestTemplate() {
         sanityCheck();
         Currency currency1 = getRestTemplate().getForObject(buildUrl, Currency.class);
+        Currency currency2 = getTestRestTemplate().getForObject(buildUrl, Currency.class);
+        assert null != currency1;
+        assert null != currency2;
+        assert currency1.equals(currency2);
+    }
+
+    public void testRestTemplate2() {
+        sanityCheck();
+        Currency currency1 = service.getCurrency("dummy");
         Currency currency2 = getTestRestTemplate().getForObject(buildUrl, Currency.class);
         assert null != currency1;
         assert null != currency2;
