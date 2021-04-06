9717047bb5af3650f7c3617513921602b48e97ec
Bob Makowski <tritonpi-git0@yahoo.com>
2021-04-06 15:53:42 -0500

    example of logger interface with a couple static methods. but @FunctionalInterface

---
+++ javaSkills/src/main/java/edu/javial/cert/se/functional/Logger.java
@@ -0,0 +1,13 @@
+package edu.javial.cert.se.functional;
+
+@FunctionalInterface
+interface Logger {
+    void log(String message);
+
+    static Logger info() {
+        return message -> System.out.println("info: " + message);
+    }
+    static Logger warning() {
+        return message -> System.out.println("warning: " + message);
+    }
+}
